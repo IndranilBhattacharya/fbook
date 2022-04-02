72b1ac15357dc3b53e0f010b54d8455825d6c2d0
Indranil Bhattacharya <ibhattacharya1218@gmail.com>
2022-04-02 15:53:21 +0530

    User Password change feature implemented

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, Subject } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { catchError, Observable, takeWhile } from 'rxjs';
 import { UserDetail } from '../../interfaces/user-detail';
 import { AppState } from '../../interfaces/app-state';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ToastService } from 'angular-toastify';
+import { userId } from 'src/app/core/selectors/user-info.selector';
 
 @Component({
   selector: 'app-settings',
@@ -10,17 +14,65 @@ import { AppState } from '../../interfaces/app-state';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit, OnDestroy {
-  isDestroyed = new Subject();
+  isAlive: boolean = true;
+  isPassResetSubmitted: boolean = false;
+  showResetPassSpinner: boolean = false;
   defaultSettingsView: string = 'profile';
   authInfo$!: Observable<UserDetail>;
+  userId: string = '';
 
-  constructor(private store: Store<AppState>) {}
+  passwordGroup: FormGroup = this.formBuilder.group({
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(
+    private store: Store<AppState>,
+    private formBuilder: FormBuilder,
+    private _toastService: ToastService,
+    private _authService: AuthenticationService
+  ) {}
 
   ngOnInit(): void {
     this.authInfo$ = this.store.select('auth');
+    this.store
+      .select(userId)
+      .pipe(takeWhile(() => this.isAlive))
+      .subscribe((id) => (this.userId = id));
   }
 
   ngOnDestroy(): void {
-    this.isDestroyed.next(true);
+    this.isAlive = false;
+  }
+
+  get passwordGroupControls() {
+    return this.passwordGroup.controls;
+  }
+
+  onPasswordReset() {
+    this.isPassResetSubmitted = true;
+    if (
+      this.passwordGroup.status === 'VALID' &&
+      this.userId &&
+      !this.showResetPassSpinner
+    ) {
+      this.showResetPassSpinner = true;
+      this._authService
+        .resetPassword(
+          this.userId,
+          this.passwordGroupControls['password'].value
+        )
+        .pipe(
+          takeWhile(() => this.isAlive),
+          catchError((err) => {
+            this.showResetPassSpinner = false;
+            throw err;
+          })
+        )
+        .subscribe(() => {
+          this.showResetPassSpinner = false;
+          this._toastService.success('Password changed successfully! 👍');
+        });
+    }
   }
 }
